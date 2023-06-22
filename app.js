bac1dd24c9ec89869bb3a53867667104c3970eb0
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-22 23:56:11 +0200

    Nedtællingsfunktion tilføjet til eventet, så man kan se antallet af ledige pladser.

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
 "use strict"
 
+let participants = [];
+
+let vacantSeats = 50;
+
 window.addEventListener("load", start);
 
 
 function start() {
     console.log("JS kører");
+    document.querySelector("#sign-up-form").addEventListener("submit", addParticipantToArray);
 }
+
+function addParticipantToArray(event) {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const age = Number(form.age.value);
+    const email = form.email.value;
+    const phone = form.phone.value;
+    const comments = form.comments.value;
+    const newParticipant = createParticipant(name, age, email, phone, comments);
+    console.log(newParticipant);
+    participants.push(newParticipant);
+    console.log(participants);
+    form.reset();
+    showNumerOfVacantSeats();
+}
+
+function createParticipant(name, age, email, phone, comments) {
+  const participant = {
+    name: name,
+    age: age,
+    email: email,
+    phone: phone,
+    comments: comments,
+  };
+  return participant;
+}
+
+function showNumerOfVacantSeats() {
+  if (vacantSeats > 0) {
+    vacantSeats--;
+    document.querySelector("#vacant-seats").textContent = vacantSeats;
+  } else {
+      
+  }
+}
+
